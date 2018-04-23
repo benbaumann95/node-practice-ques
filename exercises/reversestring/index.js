61f80b085bfb4b70e69490cdd61ac2910d09ebeb
Ben Baumann <benbaumann95@hotmail.co.uk>
2018-04-23 16:38:05 +0100

    reduce function for reverse string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,11 +6,21 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
+// function reverse(str) {
+//   return str
+//     .split('')
+//     .reverse()
+//     .join('');
+// }
+
+// function reverse(str) {
+//   return str.split('').reduce((prev, char) => char + prev, '');
+// }
+
 function reverse(str) {
-  return str
-    .split('')
-    .reverse()
-    .join('');
+  return str.split('').reduce((rev, char) => {
+    return char + rev;
+  }, '');
 }
 
 module.exports = reverse;
