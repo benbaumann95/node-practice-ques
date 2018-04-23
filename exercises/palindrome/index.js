4d7ca2b61b5dea95cb76abfece273539a5266ac4
Ben Baumann <benbaumann95@hotmail.co.uk>
2018-04-23 17:03:45 +0100

    palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,20 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+// function palindrome(str) {
+//   const reversed = str
+//     .split('')
+//     .reverse()
+//     .join('');
+//
+//   return str === reversed;
+// }
+
+function palindrome(str) {
+  const array = str.split('');
+  return array.every((char, index) => {
+    return char === str[str.length - index - 1];
+  });
+}
 
 module.exports = palindrome;
