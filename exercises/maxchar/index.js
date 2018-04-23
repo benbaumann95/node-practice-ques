632533f2910feffcd584570916c6dd440acc5b71
Ben Baumann <benbaumann95@hotmail.co.uk>
2018-04-23 20:29:56 +0100

    max character exercise

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,32 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let max = 0,
+    maxChar = '';
+  str.split('').forEach(char => {
+    if (str.split(char).length > max) {
+      max = str.split(char).length;
+      maxChar = char;
+    }
+  });
+  return maxChar;
+}
 
 module.exports = maxChar;
+
+// function maxChar(str) {
+//   let max = 0;
+//   let maxChar = '';
+//   const charMap = {};
+//   for (let char of str) {
+//     charMap[char] = charMap[char] + 1 || 1;
+//   }
+//   for (let char in charMap) {
+//   	if (charMap[char] > max) {
+//     	max = charMap[char];
+//       maxChar = char;
+//     }
+//   }
+//   return maxChar;
+// }
