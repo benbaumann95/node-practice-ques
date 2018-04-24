865129512e3848f5f203fbdf92f004dfd100087a
Ben Baumann <benbaumann95@hotmail.co.uk>
2018-04-24 13:49:30 +0100

    anagram of words

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,66 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+// function anagrams(stringA, stringB) {
+//   if (
+//     stringA
+//       .replace(/\W/g, '')
+//       .toLowerCase()
+//       .split('')
+//       .sort()
+//       .join('') ===
+//     stringB
+//       .replace(/\W/g, '')
+//       .toLowerCase()
+//       .split('')
+//       .sort()
+//       .join('')
+//   ) {
+//     return true;
+//   }
+// }
+
+function anagrams(stringA, stringB) {
+  if (cleanWords(stringA) === cleanWords(stringB)) {
+    return true;
+  }
+  return false;
+}
+
+function cleanWords(str) {
+  return str
+    .replace(/\W/g, '')
+    .toLowerCase()
+    .split('')
+    .sort()
+    .join('');
+}
+
+// function anagrams(stringA, stringB) {
+// 	const aCharMap = buildCharMap(stringA);
+//   const bCharMap = buildCharMap(stringB);
+//
+//   if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+//     return false;
+//   }
+//
+//   for (let char in aCharMap) {
+//   	if (aCharMap[char] !== bCharMap[char]) {
+//     	return false;
+//     }
+//   }
+//
+//   return true;
+// }
+//
+// function buildCharMap(str) {
+// 	const charMap = {};
+//
+//   for (let char of str.replace(/\W/g, '').toLowerCase()) {
+//   	charMap[char] = charMap[char] + 1 || 1;
+//   }
+//
+//   return charMap;
+// }
 
 module.exports = anagrams;
