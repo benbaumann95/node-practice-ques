2da504beb5a49d58105e61c15038fecc66c348a4
Ben Baumann <benbaumann95@hotmail.co.uk>
2018-04-24 12:09:20 +0100

    alternative solution

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -17,4 +17,19 @@ function chunk(array, size) {
   console.log(chunkedArray);
 }
 
+// function chunk(array, size) {
+//   let chunked = [];
+//
+//   for (let element of array) {
+//   	const last = chunked[chunked.length - 1];
+//
+//     if (!last || last.length === size) {
+//     	chunked.push([element])
+//     } else {
+//     	last.push(element)
+//     }
+//   }
+//   return chunked
+// }
+
 module.exports = chunk;
