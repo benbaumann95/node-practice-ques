527d7033c8af0a5209a3cdaf55aaced11c6541b9
Ben Baumann <benbaumann95@hotmail.co.uk>
2018-04-25 11:40:12 +0100

    return number of vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,34 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  let counter = 0;
+  const checker = ['a', 'e', 'i', 'o', 'u'];
+
+  for (let char of str.toLowerCase()) {
+    if (checker.includes(char)) {
+      counter++;
+    }
+  }
+  return counter;
+}
+
+// function vowels(str) {
+//   const m = str.match(/[aeiou]/gi);
+//   return m === null ? 0 : m.length;
+// }
+
+// function vowels(str) {
+//   let counter = 0;
+//   let vowelsList = 'aeiou'
+// 	for (let i = 0; i < str.length; i++) {
+//   	if (vowelsList.includes(str[i].toLowerCase())) {
+//     	counter ++;
+//
+//     }
+//
+//   }
+//   return counter;
+// }
 
 module.exports = vowels;
